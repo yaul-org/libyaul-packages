a837dead81dcba752134300f1d14a0332307fde1
Israel Jacquez <mrkotfw@gmail.com>
2021-02-28 22:44:55 -0800

    Add docker-pacman/update-yaul.sh

---
+++ docker-pacman/update-yaul.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+[ -z "$(docker images -q yaul-packages:latest 2>/dev/null)" ] && { printf -- "Image \"yaul-packages\" doesn't exist\n"; exit 1; } 
+
+set -e
+
+for os in "linux" "mingw-w64"; do
+    docker run -it --rm --privileged -e REPO_OS="${os}" yaul-packages:latest ./package-build-yaul.sh
+done
