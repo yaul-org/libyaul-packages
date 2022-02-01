7ace7d8e0753d269a266e0f184a5aad7d69d3c64
Israel Jacquez <mrkotfw@gmail.com>
2022-02-01 02:11:26 -0800

    Update yaul.sh

--- pacman/linux/yaul-tool-chain/yaul.sh
+++ pacman/linux/yaul-tool-chain/yaul.sh
@@ -1,6 +1,9 @@
 TOOL_CHAIN_ROOT="/opt/tool-chains/sh2eb-elf"
 [ -d "${TOOL_CHAIN_ROOT}/bin" ] && PATH="${PATH}:${TOOL_CHAIN_ROOT}/bin"
 
+[ -d "/opt/emulators/mednafen" ] && PATH="${PATH}:/opt/emulators/mednafen"
+[ -d "/opt/emulators/kronos" ] && PATH="${PATH}:/opt/emulators/kronos"
+
 export PATH
 
 YAUL_ENV_IN="${TOOL_CHAIN_ROOT}/yaul.env.in"
--- pacman/mingw-w64/yaul-tool-chain/yaul.sh
+++ pacman/mingw-w64/yaul-tool-chain/yaul.sh
@@ -1,6 +1,9 @@
 TOOL_CHAIN_ROOT="/opt/tool-chains/sh2eb-elf"
 [ -d "${TOOL_CHAIN_ROOT}/bin" ] && PATH="${PATH}:${TOOL_CHAIN_ROOT}/bin"
 
+[ -d "/opt/emulators/mednafen" ] && PATH="${PATH}:/opt/emulators/mednafen"
+[ -d "/opt/emulators/yabause" ] && PATH="${PATH}:/opt/emulators/yabause"
+
 PATH="${PATH}:/mingw64/bin"
 
 export PATH
