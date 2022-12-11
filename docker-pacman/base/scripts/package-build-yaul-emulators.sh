5ad2c46876d735a06947c8e933161914e4a922a7
Israel Jacquez <mrkotfw@gmail.com>
2022-12-10 16:33:14 -0800

    Update scripts/package-build-yaul-emulators.sh

--- docker-pacman/base/scripts/package-build-yaul-emulators.sh
+++ docker-pacman/base/scripts/package-build-yaul-emulators.sh
@@ -27,6 +27,19 @@ mingw_w64_build_yabause() {
     popd || exit 1
 }
 
+build_mednafen() {
+    export REPO_PACKAGE="yaul-emulator-mednafen"
+    export REPO_DIR="yaul-emulator-mednafen"
+    pushd ${REPO_DIR} || { panic "Directory ${REPO_DIR} doesn't exist" 1; }
+    old_pkgver=$(extract_pkgver_file "PKGBUILD")
+    if ! package_exists "${REPO_PACKAGE}" "${old_pkgver}"; then
+        make_pkg -sC
+        new_pkgver=$(extract_pkgver_file "PKGBUILD")
+        /bin/bash "${REPO_BASEPATH}/scripts/update-repo.sh" "${new_pkgver}" || exit 1
+    fi
+    popd || exit 1
+}
+
 mkdir -p "${REPO_BASEPATH}/s3"
 
 cd "${REPO_BASEPATH}" || exit 1
@@ -37,23 +50,14 @@ sync_pacman
 # This will catch a bad REPO_OS value
 cd "${REPO_BASEPATH}/repository/pacman/${REPO_OS}" || { panic "Directory path pacman/${REPO_OS} doesn't exist" 1; }
 
-export REPO_PACKAGE="yaul-emulator-mednafen"
-export REPO_DIR="yaul-emulator-mednafen"
-pushd ${REPO_DIR} || { panic "Directory ${REPO_DIR} doesn't exist" 1; }
-old_pkgver=$(extract_pkgver_file "PKGBUILD")
-if ! package_exists "${REPO_PACKAGE}" "${old_pkgver}"; then
-    make_pkg -sC
-    new_pkgver=$(extract_pkgver_file "PKGBUILD")
-    /bin/bash "${REPO_BASEPATH}/scripts/update-repo.sh" "${new_pkgver}" || exit 1
-fi
-popd || exit 1
-
 case "${REPO_OS}" in
     "linux")
         linux_build_kronos
+        build_mednafen
         ;;
     "mingw-w64")
         mingw_w64_build_yabause
+        build_mednafen
         ;;
 esac
 }
