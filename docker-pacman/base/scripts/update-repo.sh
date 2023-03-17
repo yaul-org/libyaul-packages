84ad80055eb40f558af4d9c0c97d4fbd10654236
Israel Jacquez <mrkotfw@gmail.com>
2023-03-17 16:24:36 -0700

    Fix issue with matching any arch

--- docker-pacman/base/scripts/update-repo.sh
+++ docker-pacman/base/scripts/update-repo.sh
@@ -7,23 +7,23 @@ source "${BUILD_BASEPATH}/scripts/envs.sh" || exit 1
 PKGNAME="${1}"
 PKGVER="${2}"
 PKGREL="${3}"
-PKGFILE="${PKGNAME}-${PKGVER}-${PKGREL}-${REPO_ARCH}.pkg.tar.zst"
+PKGFILE="${PKGNAME}-${PKGVER}-${PKGREL}-"'*'".pkg.tar.zst"
 
 [ -z "${PKGNAME}" ] && { panic "Argument PKGNAME is invalid" 1; }
 [ -z "${PKGVER}" ]  && { panic "Argument PKGVER is invalid" 1; }
 [ -z "${PKGREL}" ]  && { panic "Argument PKGREL is invalid" 1; }
 
-trap '/bin/rm '"${REPO_ROOTPATH}/${PKGFILE}"'' 1
+trap '/bin/rm -f '"${REPO_ROOTPATH}/"${PKGFILE}'' 1
 
-[ -f "${PKGFILE}" ] || { panic "Package file ${PKGFILE} does not exist in ${PWD}" 1; }
+[ -f ${PKGFILE} ] || { panic "Package file ${PKGFILE} does not exist in ${PWD}" 1; }
 
-/bin/rm -f "${REPO_ROOTPATH}/${PKGFILE}"
-/bin/cp "${PKGFILE}" "${REPO_ROOTPATH}"/ || { panic "Unable to copy package file to repository" 1; }
+/bin/rm -f "${REPO_ROOTPATH}/"${PKGFILE}
+/bin/cp ${PKGFILE} "${REPO_ROOTPATH}"/ || { panic "Unable to copy package file to repository" 1; }
 
 # Add package to repo. Only add packages that are not already in the database,
 # remove old package file from disk after updating database, and do not add
 # package to database if a newer version is already present
-/usr/sbin/repo-add -n -R -p "${REPO_DB}" "${REPO_ROOTPATH}/${PKGFILE}" || { panic "Unable to add package file to repository" 1; }
+/usr/sbin/repo-add -n -R -p "${REPO_DB}" "${REPO_ROOTPATH}/"${PKGFILE} || { panic "Unable to add package file to repository" 1; }
 
 # Commit changes to PKGBUILD. This happens when pkgver is updated
 if ! /usr/bin/git diff --exit-code --name-only PKGBUILD >/dev/null 2>&1; then
