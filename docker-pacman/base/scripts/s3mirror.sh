790d5b0bb6aa38a1abde49c8eb895188fc8caac6
Israel Jacquez <mrkotfw@gmail.com>
2023-03-17 16:13:00 -0700

    Exclude .gitignore file in syncing with S3 bucket

--- docker-pacman/base/scripts/s3mirror.sh
+++ docker-pacman/base/scripts/s3mirror.sh
@@ -4,5 +4,5 @@ source "${BUILD_BASEPATH}/scripts/utils.sh"
 
 [ -d "${BUILD_BASEPATH}/s3" ] || { panic "Directory path ${BUILD_BASEPATH}/s3 doesn't exist" 1; }
 
-/usr/sbin/s3cmd sync --acl-public "s3://packages.yaul.org/pacman" "${BUILD_BASEPATH}/s3/" || { panic "Unable to sync directory" 1; }
+/usr/sbin/s3cmd --exclude=.gitignore sync --acl-public "s3://packages.yaul.org/pacman" "${BUILD_BASEPATH}/s3/" || { panic "Unable to sync directory" 1; }
 }
