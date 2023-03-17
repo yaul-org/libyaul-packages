65776d819c8545653c8e48efa1c730927afbbbae
Israel Jacquez <mrkotfw@gmail.com>
2023-03-17 16:17:38 -0700

    Remove extra space

--- docker-pacman/base/scripts/s3sync.sh
+++ docker-pacman/base/scripts/s3sync.sh
@@ -9,5 +9,5 @@ SYNC_PATH=$(echo "${1}" | /bin/sed -E 's/^[\/]+//g;s/[\/]+$//g' 2>/dev/null)
 
 [ -z "${SYNC_PATH}" ] && { panic "Invalid path argument" 1; }
 
-/usr/sbin/s3cmd  --exclude=.gitignore sync --follow-symlinks --delete-removed --acl-public "${BUILD_BASEPATH}/s3/${SYNC_PATH}/" "s3://packages.yaul.org/${SYNC_PATH}/" || { panic "Unable to sync directory" 1; }
+/usr/sbin/s3cmd --exclude=.gitignore sync --follow-symlinks --delete-removed --acl-public "${BUILD_BASEPATH}/s3/${SYNC_PATH}/" "s3://packages.yaul.org/${SYNC_PATH}/" || { panic "Unable to sync directory" 1; }
 }
