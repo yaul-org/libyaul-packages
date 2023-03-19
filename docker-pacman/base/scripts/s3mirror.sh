93a659d7436889d17b6ad960c0b9813a1ad9e4ba
Israel Jacquez <mrkotfw@gmail.com>
2023-03-19 02:15:34 -0700

    Remove docker-pacman/

--- docker-pacman/base/scripts/s3mirror.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-{
-source "${BUILD_BASEPATH}/scripts/utils.sh"
-
-[ -d "${BUILD_BASEPATH}/s3" ] || { panic "Directory path ${BUILD_BASEPATH}/s3 doesn't exist" 1; }
-
-/usr/sbin/s3cmd --exclude=.gitignore sync --acl-public "s3://packages.yaul.org/pacman" "${BUILD_BASEPATH}/s3/" || { panic "Unable to sync directory" 1; }
-}
